0cef5e868506b18fd14e86032455be772e9e7cd1
张荣杰 <zzy123456>
2023-05-19 15:14:31 +0800

    feat: 实现展开与折叠功能

--- src/tree-item.vue
+++ src/tree-item.vue
@@ -2,10 +2,74 @@
 import {ref} from "vue";
 
 const props = defineProps(['item'])
+const chevron = ref < HTMLSpanElement > (null);
+const toggleExpand = (e) => {
+  chevron.value?.classList.toggle("rotate-90");
+  const element = document.getElementById(props.item.id).getElementsByClassName('node-child')
+
+  if(!element || !element[0]) return
+  element[0].classList.toggle('hide')
+}
 </script>
 
 <template>
-  <div>
-  {{item.name}}
+  <div class="d-flex align-items-center">
+    <span
+        ref="chevron"
+        class="chevron-right"
+        :class="{'hide-chevron': !item.children || item.children.length < 1}"
+        @click="toggleExpand"
+    >
+    </span>
+    <span>{{ item.name }}</span>
   </div>
 </template>
+<style scoped>
+.d-flex {
+  display: flex;
+}
+
+.align-items-center {
+  align-items: center;
+}
+
+.pointer {
+  cursor: pointer;
+}
+
+/* 隐藏箭头样式 */
+.hide-chevron {
+  display: none !important;
+}
+
+/* 箭头样式和动画 */
+.chevron-right {
+  box-sizing: border-box;
+  position: relative;
+  display: block;
+  transform: scale(1);
+  width: 22px;
+  height: 22px;
+  border: 2px solid transparent;
+  border-radius: 100px;
+  transition: .2s;
+}
+
+.chevron-right.rotate-90::after {
+  transform: rotateZ(45deg);
+}
+
+.chevron-right::after {
+  content: "";
+  display: block;
+  box-sizing: border-box;
+  position: absolute;
+  width: 7px;
+  height: 7px;
+  border-bottom: 2px solid;
+  border-right: 2px solid;
+  transform: rotate(-45deg);
+  right: 6px;
+  top: 5px
+}
+</style>
--- src/tree.vue
+++ src/tree.vue
@@ -13,15 +13,21 @@ const props = defineProps(['items'])
         :id="treeViewItem.id"
         draggable
     >
-      <tree-item
-        :item="treeViewItem"
-      ></tree-item>
-      <div v-if="treeViewItem.children?.length > 0">
-        <tree
-          :items="treeViewItem.children"
-        />
+      <tree-item :item="treeViewItem" />
+
+      <div class="node-child hide">
+        <div v-if="treeViewItem.children?.length > 0">
+          <tree
+              :items="treeViewItem.children"
+          />
+        </div>
       </div>
 
     </li>
   </ul>
 </template>
+<style scoped>
+.hide {
+  display: none;
+}
+</style>
